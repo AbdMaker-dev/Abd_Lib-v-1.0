a1fb0ce80a828f4103d5af78b9b55543ff327465
AbdMaker-dev <dioufbadaraalioune7@gmail.com>
2020-12-06 22:23:03 +0000

    premier commit

--- index.php
+++ index.php
@@ -2,17 +2,6 @@
 // Obligatoire:: toujour inclure Manager.php 
 require_once "models/Manager.php";
 
-
-$equi_dao = new AquipeDao();
-$equi_dao->add(["nom"=>"Senegale", "couleur"=>"verte"]);
-$equi_dao->add(["nom"=>"Cameroune", "couleur"=>"rouge"]);
-
-$recontre_dao = new RencontreDao();
-$recontre_dao->add(["aquipe_id"=> 1, "equipe_id"=> 2]);
-
-$match_dao = new MatchDao();
-$match_dao->add(["aquipe_id"=> 1, "equipe_id"=> 2]);
-/*
 $user_dao = new UserDao();
 
 echo "<br><br> Insertion User avec add()<br>";
@@ -21,4 +10,3 @@ var_dump($result);
 
 echo "<br><br>Slecte All user avec  All()<br>";
 var_dump($user_dao->findAll());
-*/
